8908555227efa5d88ef44f8c5d9fd95badd8cd6b
Sunil <Sunil@signitysolutions.com>
2014-02-22 09:01:24 +0530

    ddd

---
+++ ratTests/ratTests.m
@@ -0,0 +1,34 @@
+//
+//  ratTests.m
+//  ratTests
+//
+//  Created by Sunil Banjara on 22/02/14.
+//  Copyright (c) 2014 signity macbook. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ratTests : XCTestCase
+
+@end
+
+@implementation ratTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
